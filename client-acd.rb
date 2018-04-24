eb60cc5083fa44b88d0bad988dfa44cf08886b62
Ajay <ajay@dhruvsoft.com>
2018-04-24 17:02:09 +0530

    Update client-acd.rb

--- client-acd.rb
+++ client-acd.rb
@@ -335,7 +335,7 @@ post '/voicemail' do
 	#if answeredby == "human"
 	    customer_call = @client.account.calls.get(callsid)
 	    # dl_id = "13614944241"
-	    dl_id = params[:Caller]
+	    dl_id = params[:callerid]
 	    customer_call.update(:url => "http://yardidhruv-touchpoint.cs62.force.com/Click2Dial/VoiceMailDrop?uniqueid=#{dl_id}",
 			 :method => "POST")  
 	    puts customer_call.to
