b4da8aa59ba9d6e8b6858b97e54d34f4a0d31d48
Ajay <ajay@dhruvsoft.com>
2018-04-24 18:02:49 +0530

    Update client-acd.rb

--- client-acd.rb
+++ client-acd.rb
@@ -330,12 +330,13 @@ post '/voicemail' do
      child_calls.each do |childcall|
         puts "Child Call SID: #{childcall.sid}"
         callsid=childcall.sid
+	clid = childcall.from
 	#answeredby = childcall.answered_by
      end
 	#if answeredby == "human"
 	    customer_call = @client.account.calls.get(callsid)
 	    # dl_id = "13614944241"
-	    dl_id = params[:from]
+	    dl_id = params[:calid]
 	    customer_call.update(:url => "http://yardidhruv-touchpoint.cs62.force.com/Click2Dial/VoiceMailDrop?uniqueid=#{dl_id}",
 			 :method => "POST")  
 	    puts customer_call.to
