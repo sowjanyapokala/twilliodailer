678065b4f66e6f2adc48319100f4b7b83387f6cd
Jothieswari <mjothieswari29@gmail.com>
2018-06-18 13:12:57 +0530

    Default caller id on softphone

--- public/scripts/softphone.js
+++ public/scripts/softphone.js
@@ -281,8 +281,16 @@ SP.functions.updateAgentStatusText = function(statusCategory, statusText, inboun
 }
 
 // Call button will make an outbound call (click to dial) to the number entered 
-$("#action-buttons > button.call").click( function( ) {
-  params = {"PhoneNumber": $("#number-entry > input").val(), "CallerId": $("#callerid-entry > input").val()};
+$("#action-buttons > button.call").click( function( ) { 
+  var callerPhNumber = "";
+  callerPhNumber = cleanFormatting($("#number-entry > input").val());
+  if($("#callerid-entry > input").val() == null || $("#callerid-entry > input").val() == undefined ){
+	  
+  params = {"PhoneNumber": callerPhNumber, "CallerId": '8442012921'};
+  }
+  else{
+	  params = {"PhoneNumber": callerPhNumber, "CallerId": $("#callerid-entry > input").val()}; 
+	  } 
   Twilio.Device.connect(params);
 });
 
