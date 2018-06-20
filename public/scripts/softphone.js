b5f95dd1824b68ca64b40e1a3f54732bb660b17c
Jothieswari <mjothieswari29@gmail.com>
2018-06-20 16:11:13 +0530

    Commit

--- public/scripts/softphone.js
+++ public/scripts/softphone.js
@@ -283,11 +283,11 @@ SP.functions.updateAgentStatusText = function(statusCategory, statusText, inboun
 // Call button will make an outbound call (click to dial) to the number entered 
 $("#action-buttons > button.call").click( function() { 
   var callerPhNumber = "";
-  callerPhNumber = cleanFormatting($("#number-entry > input").val());
-  if($("#callerid-entry > input").val() == null || $("#callerid-entry > input").val() == undefined || $("#callerid-entry > input").val() == ''){
+  callerPhNumber = cleanFormatting($("#number-entryIndex > input").val());
+  if($("#callerid-entryIndex > input").val() == null || $("#callerid-entryIndex > input").val() == undefined || $("#callerid-entryIndex > input").val() == ''){
 	 
   params = {"PhoneNumber": callerPhNumber, "CallerId": '3019005961'};//8442012921
-  $("#callerid-entry > input").val("8442012921");
+  $("#callerid-entryIndex > input").val("8442012921");
  // $("#callerid-entry > input").attr("disabled", "disabled");
   }
   else{
