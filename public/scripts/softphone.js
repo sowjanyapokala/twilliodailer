e6972b7fb62b526b58363a84067e315dc6bbdcab
Jothieswari <mjothieswari29@gmail.com>
2018-06-20 15:14:50 +0530

    Commit

--- public/scripts/softphone.js
+++ public/scripts/softphone.js
@@ -544,8 +544,8 @@ var callStartCall = function(response) {
   //Twilio.Device.connect(params);
   var w = 200;//screen.width/2;
                 var h = 400;//screen.height/2;
-                var l = (screen.width)-(w-25);
-                var t = (screen.height)-(h-17);
+                var l = (screen.width)-(w-20);
+                var t = (screen.height)-(h-10);
   var strWindowFeatures = "top="+t+",left="+l+",width="+w+",height="+h+",menubar=no,location=no,resizable=yes,scrollbars=no,status=no";
   var url = "https://twilioctiapp.herokuapp.com/home?phoneNumber="+callerPhoneNumber+"&CallerId="+response.result;
                 
