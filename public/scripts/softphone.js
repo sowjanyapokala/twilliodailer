c570b3d5c9af976c6419be34847a05f583f1c495
Yetender Poonia <yetenderpoonia@192.168.1.6>
2018-06-21 09:10:52 +0530

    Validating CallerId

--- public/scripts/softphone.js
+++ public/scripts/softphone.js
@@ -293,7 +293,7 @@ $("#action-buttons > button.call").click( function() {
   }
   params = {"PhoneNumber": callerPhNumber, "CallerId": dialerCallerId};
   console.log('Before calling Params===>'+JSON.stringify(params));
-  //Twilio.Device.connect(params);
+  Twilio.Device.connect(params);
 });
 
 // Hang up button will hang up any active calls
