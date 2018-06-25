605d27d49688e644cf7a17592c0440b09b7ff71f
Yetender Kumar <yetenderkumar@Jagadeesh-MacBook-Air-2.local>
2018-06-25 12:23:11 +0530

    Call log

--- public/scripts/softphone.js
+++ public/scripts/softphone.js
@@ -449,8 +449,7 @@ var saveLogData = {};
 saveLogData['phoneNumber'] = document.getElementById("phoneNumberInput").value;
 saveLogData['callerId'] = document.getElementById("callerIdInput").value;
 saveLogData['objectId'] = document.getElementById("objectId").value;  
-alert('Before calling save()');
-saveLog(saveLogData);
+sforce.interaction.runApex('CallerTasklogService', 'generateCallLog', 'logParamsMap='+JSON.stringify(saveLogData), saveLogcallback);
 }
 }
 
